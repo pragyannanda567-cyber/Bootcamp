795343b730446893febbb664c5cf9a1294c823af
Pragyan Nanda <pragyannanda567@gmail.com>
2026-06-29 10:27:54 +0530

    Week 2 work

---
+++ Week2/day5/src/App.jsx
@@ -0,0 +1,16 @@
+import Card from './components/Card.jsx'
+
+
+function App(){
+  return (
+    <>
+      <div>
+        <h1>Hello World</h1>
+        <Card/>
+      </div>
+      <h1>Hello</h1>
+    </>
+      )
+}
+
+export default App;
